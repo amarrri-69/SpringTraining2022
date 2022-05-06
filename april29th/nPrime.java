766c7221bf49a2ad8255337e2e442813bc3b44b5
amarrri-69 <98570914+amarrri-69@users.noreply.github.com>
2022-05-05 20:17:38 -0400

    accidentally did first n odd numbers but it's too late

--- april29th/nPrime.java
+++ april29th/nPrime.java
@@ -1,4 +1,4 @@
-public static void nPrime(int number) {
+public static void nOdd(int number) {
   for (int i=0; i<number; i++) {
     int difference = (i+1)*(i+1) - i * i;
     System.out.println(difference);
