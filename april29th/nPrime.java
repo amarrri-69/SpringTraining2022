4c035a1187af693ef833092fbc84c120479de1e3
amarrri-69 <98570914+amarrri-69@users.noreply.github.com>
2022-05-30 11:19:52 -0400

    Update nPrime.java

--- april29th/nPrime.java
+++ april29th/nPrime.java
@@ -1,6 +1,34 @@
-public static void nOdd(int number) {
-  for (int i=0; i<number; i++) {
-    int difference = (i+1)*(i+1) - i * i;
-    System.out.println(difference);
-  }
+import java.util.ArrayList;
+
+public static boolean isPrime(int possiblePrime) {
+    if (possiblePrime == 1) {
+        return false;
+    }
+        
+    if (possiblePrime == 2) {
+        return true;
+    }
+        
+    for (int i=2; i<possiblePrime; i++){
+        if (possiblePrime % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+    
+public static void nPrime(int x) {
+    ArrayList<Integer> pList = new ArrayList<Integer>();
+    int possiblePrime = 2;
+    while (true) {
+        if (isPrime(possiblePrime)) {
+            pList.add(possiblePrime);
+        }
+        possiblePrime++;
+        if (pList.size() == x) {
+            break;
+        }
+    }
+        
+    System.out.println(pList);
 }
