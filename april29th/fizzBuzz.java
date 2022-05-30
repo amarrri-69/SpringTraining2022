9b8b515d3986d229dfb72e72db60cc055d930bba
amarrri-69 <98570914+amarrri-69@users.noreply.github.com>
2022-05-30 12:25:55 -0400

    fix camel case

--- april29th/fizzBuzz.java
+++ april29th/fizzBuzz.java
@@ -1,16 +1,18 @@
- public static void fizzBuzz(int number) {
-   for (int i=1; i <= number; i++) {
-     if ((i % 3 == 0) && (i % 5 != 0)) {
-       System.out.println("fizz");
-     }
-     else if ((i % 3 != 0) && (i % 5 == 0)) {
-       System.out.println("buzz");
-     }
-     else if (i % 15 == 0) {
-       System.out.println("fizz buzz");
-     }
-     else {
-       System.out.println(i);
-     }
-   }
- }
+public class FizzBuzz {
+    public static void fizzBuzz(int number) {
+        for (int i=1; i <= number; i++) {
+            if ((i % 3 == 0) && (i % 5 != 0)) {
+                System.out.println("fizz");
+            }
+            else if ((i % 3 != 0) && (i % 5 == 0)) {
+                System.out.println("buzz");
+            }
+            else if (i % 15 == 0) {
+                System.out.println("fizz buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
