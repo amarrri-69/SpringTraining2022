13f585b44dffae866238339188363eaf81b90a9c
amarrri-69 <98570914+amarrri-69@users.noreply.github.com>
2022-05-30 12:29:01 -0400

    fix camel case

--- april29th/nPrime.java
+++ april29th/nPrime.java
@@ -1,34 +1,35 @@
 import java.util.ArrayList;
 
-public static boolean isPrime(int possiblePrime) {
-    if (possiblePrime == 1) {
-        return false;
-    }
-        
-    if (possiblePrime == 2) {
-        return true;
-    }
-        
-    for (int i=2; i<possiblePrime; i++){
-        if (possiblePrime % i == 0) {
+public class Prime {
+    public static boolean isPrime(int possiblePrime) {
+        if (possiblePrime == 1) {
             return false;
         }
-    }
-    return true;
-}
-    
-public static void nPrime(int x) {
-    ArrayList<Integer> pList = new ArrayList<Integer>();
-    int possiblePrime = 2;
-    while (true) {
-        if (isPrime(possiblePrime)) {
-            pList.add(possiblePrime);
-        }
-        possiblePrime++;
-        if (pList.size() == x) {
-            break;
-        }
-    }
         
-    System.out.println(pList);
+        if (possiblePrime == 2) {
+            return true;
+        }
+        
+        for (int i=2; i<possiblePrime; i++){
+            if (possiblePrime % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void nPrime(int x) {
+        ArrayList<Integer> pList = new ArrayList<Integer>();
+        int possiblePrime = 2;
+        while (true) {
+            if (isPrime(possiblePrime)) {
+            pList.add(possiblePrime);
+            }
+            possiblePrime++;
+            if (pList.size() == x) {
+                break;
+            }
+        }
+        System.out.println(pList);
+    }
 }
